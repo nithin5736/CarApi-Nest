0a4ed4b0e332088104b29c2c483714cc27bcc336
nithin5736 <gubbasainithin123@gmail.com>
2023-12-20 14:54:16 +0530

    complete end-2-end testing

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,18 +7,28 @@ import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
-const cookieSession = require('cookie-session')
+import { ConfigModule, ConfigService } from '@nestjs/config';
+const cookieSession = require('cookie-session');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db.sqlite',
-      entities: [User, Report],
-      synchronize: true
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          type: 'sqlite',
+          database: config.get<string>('DB_NAME'),
+          entities: [User, Report],
+          synchronize: true,
+        };
+      },
     }),
     UsersModule,
-    ReportsModule
+    ReportsModule,
   ],
   controllers: [AppController],
   providers: [
@@ -26,15 +36,19 @@ const cookieSession = require('cookie-session')
     {
       provide: APP_PIPE,
       useValue: new ValidationPipe({
-        whitelist: true
-      })
-    }
+        whitelist: true,
+      }),
+    },
   ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(cookieSession({
-      keys: ["abcdefghijklmnopqrstuvwxyz"]
-    })).forRoutes('*')
+    consumer
+      .apply(
+        cookieSession({
+          keys: ['abcdefghijklmnopqrstuvwxyz'],
+        }),
+      )
+      .forRoutes('*');
   }
 }
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -5,26 +5,41 @@ import { AppModule } from './../src/app.module';
 // import { setUpApp } from '../src/setup-app';
 
 describe('Authentication System', () => {
-    let app: INestApplication;
+  let app: INestApplication;
 
-    beforeEach(async () => {
-        const moduleFixture: TestingModule = await Test.createTestingModule({
-            imports: [AppModule],
-        }).compile();
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
 
-        app = moduleFixture.createNestApplication();
-        // setUpApp(app);
-        await app.init();
-    });
+    app = moduleFixture.createNestApplication();
+    // setUpApp(app);
+    await app.init();
+  });
 
-    it('handles a signup request', async () => {
-        const email = 'gubb14@gmail.com'
-        const res = await request(app.getHttpServer())
-            .post('/auth/signup')
-            .send({ email, password: 'asdf123' })
-            .expect(201);
-        const { id, email: email_1 } = res.body;
-        expect(id).toBeDefined();
-        expect(email_1).toEqual(email_1);
-    });
+  it('handles a signup request', async () => {
+    const email = 'gubb14@gmail.com';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf123' })
+      .expect(201);
+    const { id, email: email_1 } = res.body;
+    expect(id).toBeDefined();
+    expect(email_1).toEqual(email);
+  });
+
+  it('signup request followed by which user is currently signed in', async () => {
+    const email = 'asdf@gmail.com';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+    const cookie = res.get('Set-Cookie');
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual('asdf@gmail.com');
+  });
 });
